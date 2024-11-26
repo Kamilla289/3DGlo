e0a9ca042fc7f6e5cacfd67cf7a3cca461f6e34a
Kamilla289 <kami.rivay56@gmail.com>
2024-11-26 19:35:37 +0500

    add: helpers animate

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,35 +1,40 @@
+import { animate } from './helpers'
+
 const modal = () => {
   const buttons = document.querySelectorAll('.popup-btn');
   const modal = document.querySelector('.popup');
-
   let opacity = 0;
   modal.style.opacity = opacity;
 
-
+  
   const fadeIn = () => {
     if (document.documentElement.clientWidth > 768) {
-      modal.style.display = 'block';
-      opacity += 0.1;
-      modal.style.opacity = opacity;
-
-      if(opacity < 1) {
-        requestAnimationFrame(fadeIn);
-      }
-    } else {
-      modal.style.display = 'block';
-      modal.style.opacity = 1;
+      animate({
+        duration: 300,
+        timing(timeFraction) {
+          return timeFraction;
+        },
+        draw(progress) {
+          modal.style.display = 'block';
+          modal.style.opacity = progress;
+        }
+      });
     }
-
-  };
-
-  const fadeOut = () => {
-    opacity -= 0.1;
-    modal.style.opacity = opacity;
-    if(opacity > 0) {
-      requestAnimationFrame(fadeOut);
-    } else {
-      modal.style.display = 'none';
     };
+    
+  const fadeOut = () => {
+    if (document.documentElement.clientWidth > 768) {
+      animate({
+        duration: 300,
+        timing(timeFraction) {
+          return timeFraction;
+        },
+        draw(progress) {
+          modal.style.display = 'none';
+          modal.style.opacity = progress;
+        }
+      });
+    }
   };
 
   buttons.forEach(btn => {
@@ -43,6 +48,18 @@ const modal = () => {
         fadeOut();
       }
    });
+
+  buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+      modal.style.display = 'block';
+    });
+  });
+  
+  modal.addEventListener('click', (e) => {
+      if (!e.target.closest('.popup-content') || e.target.classList.contains('popup-close')) {
+        modal.style.display = 'none';
+      }
+  });
 };
 
 export default modal
